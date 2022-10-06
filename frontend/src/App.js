750b38d92eb20a2aa9829154997c9b00f879fd44
C-Skala <cjskala@gmail.com>
2022-10-05 22:19:14 -0400

    made post and table functions for weight

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 //import FoodPage from "./pages/Food/foodPage"
 //import HeartRatePage from "./pages/Heart Rate/heartRatePage"
 //import MedicationsPage from "./pages/Medications/MedicationsPage"
-//import WeightPage from "./pages/Weight/weightPage"
+import WeightPage from "./pages/Weight/weightPage"
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -124,7 +124,6 @@ function App() {
 
   useEffect(() => {
     async function getAllWeight(){
-      try {
         let response = await axios.get('http://127.0.0.1:8000/api/Weight/', {
           headers: {
             Authorization: "Bearer " + token,
@@ -132,74 +131,15 @@ function App() {
         });
         setWeight(response.data);
         console.log(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
     };
     getAllWeight();
   }, [token]);
 
-
-
-
-
-
-
-//  useEffect(() => {
-//      getAllBloodPressure(); 
-//      getAllBloodSugar(); 
-//      getAllFood(); 
-//      getAllHeartRate(); 
-//      getAllMedications(); 
-//      getAllWeight(); 
-//  }, []);
-
-//  async function getAllBloodPressure(){
-//      const response = await axios.get('http://127.0.0.1:8000/api/BP/')
-//      setBloodPressure(response.data)
-//      console.log(response.data)
-//  }
-
- // async function getAllBloodSugar(){
- //     const response = await axios.get('http://127.0.0.1:8000/api/BS/')
- //     setBloodSugar(response.data)
- //     console.log(response.data)
- // }
-
-  //async function getAllFood(){
-  //    const response = await axios.get('http://127.0.0.1:8000/api/food/')
-  //    setFood(response.data)
-  //    console.log(response.data)
- // }
-
-  //async function getAllHeartRate(){
-  //    const response = await axios.get('http://127.0.0.1:8000/api/HR/')
-  //    setHeartRate(response.data)
-  //    console.log(response.data)
- // }
-
-  //async function getAllMedications(){
-  //    const response = await axios.get('http://127.0.0.1:8000/api/meds/')
-  //    setMedications(response.data)
-   //   console.log(response.data)
-  //}
-
-  //async function getAllWeight(){
-  //    const response = await axios.get('http://127.0.0.1:8000/api/weight/')
-  //    setWeight(response.data)
-  //    console.log(response.data)
- // }
-  
-  
-  
  //<Route path="/food" element={<FoodPage />} />
  //<Route path="/medications" element={<MedicationsPage />} />
  //<Route path="/blood pressure" element={<BloodPressurePage />} />
  //<Route path="/blood sugar" element={<BloodSugarPage />} />
  //<Route path="/heart rate" element={<HeartRatePage />} />
- //<Route path="/weight" element={<WeightPage />} />
-  
-  
   
   
   return (
@@ -216,7 +156,7 @@ function App() {
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        
+        <Route path="/weight" element={<PrivateRoute><WeightPage parentWeight = {weight} /></PrivateRoute>} />
       </Routes>
       <Footer />
     </div>
--- frontend/src/pages/Weight/weightPage.jsx
+++ frontend/src/pages/Weight/weightPage.jsx
@@ -1,28 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
+import useCustomForm from '../../hooks/useCustomForm';
+
+let initialValues = {
+  weight: "",
+  date: "",
+  comments:""
+};
 
 
 const WeightPage = (props) => {
-    return ( 
-        <table className = 'table'>
-        <thead>
-          <tr>
-            <th>Entry Number</th>
-            <th>Weight</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.parentEntries.map((entry, index) => {
-            return(
-              <tr key={index}>
-                <td>{index +1}</td>
-                <td>{entry.weight}</td>
-                <td>{entry.date}</td>
+  const [user, token] = useAuth();  
+  const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, postNewWeight)
+
+
+  async function postNewWeight(){
+    const response = await axios.post('http://127.0.0.1:8000/api/Weight/', formData, {
+      headers: {
+        Authorization: "Bearer " + token,
+      }
+    })
+    console.log(response.data)
+   }
+  
+   function refreshPage() {
+    window.location.reload(false);
+  }
+  
+  
+  
+  
+  
+  return ( 
+      <div>
+        <div>
+          <table className = 'table'>
+            <thead>
+              <tr>
+                <th>Weight</th>
+                <th>Date</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+              <tbody>
+                {props.parentWeight.map((weight, index) => {
+                return(
+                <tr key={index}>
+                  <td>{weight.weight}</td>
+                  <td>{weight.date}</td>
+                </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div>
+            <form onSubmit = {handleSubmit}>
+                <label>Weight:{" "}</label>
+                  <input type = 'number' name = 'weight' value = {formData.weight} onChange = {handleInputChange}/>
+                <label>Date:{" "}</label>
+                  <input type = 'date' name = 'date' value = {formData.date} onChange = {handleInputChange}/>
+                <label>comments:{" "}</label>
+                  <input type = "text" name = 'comments' value = {formData.comments} onChange = {handleInputChange}/>
+                <button onClick={refreshPage}>submit</button>
+            </form>
+        </div> 
+      </div>
+        
      );
 }
  
